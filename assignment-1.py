38779b92834dd56b98e58e537cf3eaedab48ea55
Aryan-jr-07 <aryanjr2010@gmail.com>
2025-02-24 23:50:50 +0530

    updated

--- assignment-1.py
+++ assignment-1.py
@@ -1,6 +1,6 @@
 name = input("enter your name")
 marks = input("enter your marks")
 percentage = input("enter your percentage")
-print(f"name:{name} \nmarks:{marks} \npercentage:{percentage} ")
+print(f"name:{name} \nmarks:{marks} \n percentage:{percentage} ")
 
 
