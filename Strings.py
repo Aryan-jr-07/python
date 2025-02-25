b60b867c57c23b0d570d2b0f581503a0fda351e3
Aryan-jr-07 <aryanjr2010@gmail.com>
2025-02-26 02:48:36 +0530

    updated

---
+++ Strings.py
@@ -0,0 +1,38 @@
+# name = "aryan"
+# age =16
+# print("My name is %s and I'm %d " %(name, age))
+
+# print("my name is {name} and I'm {age} and I live in {city}".format(name = "aryan",age=15,city ="a.bad"))
+
+# my_name = "Aryan"
+# print(my_name)
+# print(my_name[::3])
+# print(my_name[0])
+# print(my_name[1])
+# print(my_name[2])
+# print(my_name[3])
+# print(my_name[4])
+# print(my_name[-1])
+# print(my_name[-2])
+# print(my_name[-3])
+# print(my_name[-4])
+# print(my_name[-5])
+
+word ="hello, world"
+
+'''
+print(len(word))
+print(word.upper())  # upper case (capital letter)
+print(word.lower())  # lower case (small letter)
+print(word.count("r"))   # let u know how many time does that letter comes in that strings
+print(word.find("h"))    # this will let uk ki kitne index no. pe hai wo word.
+print(word.split(','))
+print(word.split()) 
+print(word.replace("hello", "nameste"))    # this will replace the hello word with nameste
+print(word.title()) '''    # this will make the word to title
+
+# word2 = "    hello world     "
+# print(len(word2))
+# print(word2.strip())
+zwords = ("Aryan","is","great")
+print(" ".join(zwords))
