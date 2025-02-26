88bca486c0b3d73be5a675fcc9b17e21924e6835
Aryan-jr-07 <aryanjr2010@gmail.com>
2025-02-26 19:31:36 +0530

    updated

---
+++ loops.py
@@ -0,0 +1,30 @@
+# while loop
+'''
+count = 0
+while  count <=4:
+  print(count)
+  count = count +1
+
+count = 5
+while  count >=0:
+  print(count)
+  count = count -1
+else:
+  print("while loop ended") '''
+
+#for loop
+# language = "python"
+# for x in language:
+#   print(x)
+
+#range(stop(exclusive))
+# range(start,stop,step)
+
+# for i in range(5):
+#   print(i)
+
+# for i in range(5,10):
+#   print(i)
+
+for i in range(1,10,2):
+  print(i)  
