f440c52cf7d20eb5c031317d552ae36e54ea5be6
aryan-jr-07 <aryanjr2010@gmail.com>
2025-06-09 23:24:57 +0530

    updated

--- Strings.py
+++ Strings.py
@@ -1,10 +1,12 @@
-# name = "aryan"
+name = "aryan"
 # age =16
 # print("My name is %s and I'm %d " %(name, age))
 # %s Format specifier for an string AND %d Format specifier for an integer
 
 
 # print("my name is {name} and I'm {age} and I live in {city}".format(name = "aryan",age=15,city ="a.bad"))
+# print("my name is {name} and I'm {age} and I live in {city}".format(name = "aryan",age=15,city ="a.bad"))
+# print(f"my name is {name} hehe ")
 
 # my_name = "Aryan"
 # print(my_name)
