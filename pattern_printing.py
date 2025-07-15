13f45e285f485db2d1aa3142c2a2252aeb07e358
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-15 23:53:14 +0530

    updated

---
+++ pattern_printing.py
@@ -0,0 +1,26 @@
+# Left-Aligned Right-Angled Triangle
+'''n =10
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()'''
+
+# shortcut
+'''n =6
+for i in range(1,n+1):
+    print(" * "*i)'''
+
+
+
+
+# inverted Triangle
+'''n = 50
+for i in range(n,0,-1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()'''
+
+# shortcut
+'''n= 10
+for i in range(n,0,-1):
+    print(" * "*i)'''
