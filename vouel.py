e7a3e8273cf034b7e897f16d67d657cfbb1508f0
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-16 21:13:32 +0530

    comnt

--- vouel.py
+++ vouel.py
@@ -1,2 +1,8 @@
-my_string ="My Name is Aryan And what's your name?"
-vowel = "aeiou"
+# my_string ="My Name is Aryan And what's your name?"
+# vowel = "aeiou"
+# count =1
+
+# for char in my_string:
+#     if char.lower() in vowel:
+#         count +=1
+# print("total vowels are", count)
