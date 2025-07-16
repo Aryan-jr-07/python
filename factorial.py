8db90339f735c219c283b6136bf4bcce6d94b802
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-16 21:06:28 +0530

    udpated

--- factorial.py
+++ factorial.py
@@ -1,7 +1,7 @@
-def factorial(n):
-    res =1
-    while n>0:
-        res *= n
-        n -=1
-    return res
-print(factorial(5))
+# def factorial(n):
+#     res =1
+#     while n>0:
+#         res *= n
+#         n -=1
+#     return res
+# print(factorial(5))
