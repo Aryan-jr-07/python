cb3543789c8f2efe123ee8f5961aa1094c2ee831
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-19 21:12:26 +0530

    updated

--- tuples.py
+++ tuples.py
@@ -1,4 +1,11 @@
 '''Tuples'''
 # tuple are immutable means it is not changeble
 
-#  mutliple way to creare tuples 
+'''mutliple way to creare tuples'''
+
+# 1 using pranthesis -- just write inside small bracket
+# hehe = (1,2,[99,27],(3,4),"siuu")
+
+# 2 without pranthesis -- just write anything it should be comma seperated
+# hehe = 1,2,3,"siuu",99
+
