d3101f460df90499e0fe869921bb0b3a37a419f7
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-19 23:25:59 +0530

    set

--- set.py
+++ set.py
@@ -2,3 +2,7 @@
 '''It is a collection of unique items 
     does not allow duplicates items and 
     it cannot be indexed'''
+
+# charcteristics of sets
+''' unorderd : elements have no definded order, you cannont access element by indexing. '''
+''' unique element : no duplicates'''
